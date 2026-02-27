2bd43526529ea660bced6237e4f80e017d2cc68c
Rishi Aitha <rishi.aitha@gmail.com>
2026-02-27 16:33:52 -0500

    fixed ball directions?

--- src/game.js
+++ src/game.js
@@ -246,8 +246,8 @@ export default {
 
                                     // Convert perpMovement to screen 2D force
                                     // Use the right and up vectors we calculated earlier
-                                    const forceX = -perpMovement.dot(up); // Negative because screen Y is down
-                                    const forceY = perpMovement.dot(right);
+                                    const forceX = -perpMovement.dot(right);
+                                    const forceY = perpMovement.dot(up);
                                     
                                     // Send force to screen
                                     context.sendGameMessage({
